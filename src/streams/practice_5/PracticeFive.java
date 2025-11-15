070d3ef44c2a87434ad1a1f07aa1ca669a4d0973
Мун Александр Илларионович <a.mun@alataucitybank.kz>
2025-11-15 13:59:31 +0500

    [MODERN-JAVA][CHAPTER-5-8-5] fibobacci

--- src/streams/practice_5/PracticeFive.java
+++ src/streams/practice_5/PracticeFive.java
@@ -1,15 +1,21 @@
 package streams.practice_5;
 
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class PracticeFive {
     public static void main(String[] args) {
-        fibonacciSequence();
+        iterateWithLimit();
     }
 
     public static void fibonacciSequence() {
         Stream.iterate(new int[]{0,1}, n -> new int[]{n[1], n[0] + n[1]})
                 .limit(20)
                 .forEach(n -> System.out.println("(" + n[0] + ", " + n[1] + ")"));
+    }
+
+    public static void iterateWithLimit() {
+        IntStream.iterate(0, n -> n < 100, n -> n + 4)
+                .forEach(System.out::println);
     }
 }
